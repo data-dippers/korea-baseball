17cb142f6524bb088f4b6639f0925567a5abba22
CoodingPenguin <coodingpenguin@gmail.com>
2024-04-30 21:34:05 +0900

    Create base bigquery etl job class

--- etl/bigquery/base.py
+++ etl/bigquery/base.py
@@ -1,47 +1,111 @@
+from enum import StrEnum
 from typing import Literal
 
 from abc import ABCMeta, abstractmethod
 
 import pandas as pd
-from google.cloud import bigquery
-from pandas import DataFrame
+from pydantic import BaseModel, RootModel
+
+
+class FieldType(StrEnum):
+    """
+    field type class
+    """
+    string = "STRING"
+    bytes = "BYTES"
+    integer = "INTEGER"
+    float = "FLOAT"
+    boolean = "BOOLEAN"
+    timestamp = "TIMESTAMP"
+    date = "DATE"
+    time = "TIME"
+    datetime = "DATETIME"
+    geography = "GEOGRAPHY"
+    numeric = "NUMBERIC"
+    bignumeric = "BIGNUMERIC"
+    json = "JSON"
+    record = "RECORD"
+    range = "RANGE"
+
+
+class Column(BaseModel):
+    """
+    column class
+    """
+    name: str
+    type: FieldType
+
+
+class Schema(RootModel):
+    """
+    schema class
+    """
+    root: list[Column]
+
+    def __iter__(self):
+        return iter(self.root)
+
+    def __getitem__(self, item):
+        return self.root[item]
+
+    @property
+    def names(self) -> list[str]:
+        return [column.name for column in self.root]
+
+    def to_dict(self) -> list[dict[str, FieldType]]:
+        return [column.model_dump() for column in self.root]
 
 
 class BaseJob(metaclass=ABCMeta):
     """
-    base job class for bigquery etl
+    base job class
+
+    :param project_id: The ID of the BiqQuery project.
+    :param table_id: The ID of the BigQuery dataset table.
+    :param schema: The schema of the BigQuery dataset table.
+    :param chunk_size: Number of rows to be inserted in each chunk from dataframe.
+        Set to None to load the whole dataframe at once.
+    :param if_exists: Behavior when the table exists.
+        'fail': If table exists raise pandas_gbq.gbq.TableCreationError.
+        'replace': If table exists, drop it, recreate id, and insert data.
+        'append': If table exists, insert data. Create if does not exist.
     """
 
     def __init__(
-        self,
-        destination_table: str,
-        if_exists: Literal["fail", "replace", "append"],
+            self,
+            project_id: str,
+            table_id: str,
+            schema: Schema,
+            chunk_size: int | None = None,
+            if_exists: Literal["fail", "replace", "append"] = "append"
     ):
-        self.destination_table = destination_table
+        self.project_id = project_id
+        self.table_id = table_id
+        self.schema = schema
+        self.chunk_size = chunk_size
         self.if_exists = if_exists
 
+    def __repr__(self) -> str:
+        return f"<cls:{self.__class__.__name__}>"
+
     @abstractmethod
-    def extract(self) -> dict:
+    def extract(self) -> pd.DataFrame:
         pass
 
     @abstractmethod
-    def transform(self, data: dict) -> DataFrame:
+    def transform(self, df: pd.DataFrame) -> pd.DataFrame:
         pass
 
-    @abstractmethod
-    def define(self) -> list[dict[str, str]]:
-        pass
-
-
-    def load(self, df: DataFrame, table_schema: list[dict[str, str]]):
+    def load(self, df: pd.DataFrame):
         df.to_gbq(
-            destination_table=self.destination_table,
+            destination_table=self.table_id,
+            project_id=self.project_id,
+            chunksize=self.chunk_size,
             if_exists=self.if_exists,
-            table_schema=table_schema
+            table_schema=self.schema.to_dict()
         )
 
     def run(self):
-        data = self.extract()
-        df = self.transform(data)
-        schema = self.define()
-        self.load(df, schema)
+        df = self.extract()
+        df = self.transform(df)
+        self.load(df)
